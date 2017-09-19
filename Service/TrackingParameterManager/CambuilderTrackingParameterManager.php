f7d9b9144ecf6561c217e8928071a9db1f05d228
Olivier Maisonneuve <olivierm@ex-situ.com>
2017-09-19 15:48:50 -0400

    extract from query only and default value

--- Service/TrackingParameterManager/CambuilderTrackingParameterManager.php
+++ Service/TrackingParameterManager/CambuilderTrackingParameterManager.php
@@ -3,16 +3,16 @@
 namespace EXS\LanderTrackingCambuilderBundle\Service\TrackingParameterManager;
 
 use Symfony\Component\HttpFoundation\ParameterBag;
-use Symfony\Component\HttpFoundation\Request;
-use EXS\LanderTrackingHouseBundle\Service\TrackingParameterManager\TrackingParameterExtracterInterface;
 use EXS\LanderTrackingHouseBundle\Service\TrackingParameterManager\TrackingParameterFormatterInterface;
+use EXS\LanderTrackingHouseBundle\Service\TrackingParameterManager\TrackingParameterInitializerInterface;
+use EXS\LanderTrackingHouseBundle\Service\TrackingParameterManager\TrackingParameterQueryExtracterInterface;
 
 /**
  * Class CambuilderTrackingParameterManager
  *
  * @package EXS\LanderTrackingCambuilderBundle\Service\TrackingParameterManager
  */
-class CambuilderTrackingParameterManager implements TrackingParameterExtracterInterface, TrackingParameterFormatterInterface
+class CambuilderTrackingParameterManager implements TrackingParameterQueryExtracterInterface, TrackingParameterFormatterInterface, TrackingParameterInitializerInterface
 {
     /**
      * @var int
@@ -32,20 +32,17 @@ class CambuilderTrackingParameterManager implements TrackingParameterExtracterIn
     /**
      * {@inheritdoc}
      */
-    public function extract(Request $request)
+    public function extractFromQuery(ParameterBag $query)
     {
         $trackingParameters = [];
 
         if (
-            (null !== $afno = $request->query->get('AFNO'))
+            (null !== $afno = $query->get('AFNO'))
             && (preg_match('`^1-(?<cmp>[a-z0-9]+)-(?<exid>[a-z0-9]+)$`i', $afno, $matches))
         ) {
             /** Get 'cmp' and 'exid' from 'AFNO' query parameter. */
             $trackingParameters['cmp'] = $matches['cmp'];
             $trackingParameters['exid'] = $matches['exid'];
-        } elseif ($request->cookies->has('exid')) {
-            $trackingParameters['cmp'] = $request->cookies->get('cmp', $this->defaultCmp);
-            $trackingParameters['exid'] = $request->cookies->get('exid');
         }
 
         return $trackingParameters;
@@ -61,13 +58,23 @@ class CambuilderTrackingParameterManager implements TrackingParameterExtracterIn
         if ($trackingParameters->has('exid')) {
             $afno = sprintf(
                 '1-%s-%s',
-                $trackingParameters->get('cmp', $this->defaultCmp),
+                $trackingParameters->get('cmp'),
                 $trackingParameters->get('exid')
             );
         }
 
         return [
             'AFNO' => $afno,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function initialize()
+    {
+        return [
+            'cmp' => $this->defaultCmp,
         ];
     }
 }
